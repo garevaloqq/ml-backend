9bf2c0684e0836994513348abd23a4c3e7ecbabd
Geider <garevaloqq@gmail.com>
2022-12-12 19:54:44 -0300

    Add method findMostFrequent

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,10 @@
+/**
+ * Get categoría que mas se repite en la búsqueda de productos
+ *
+ * @param arr - Arreglo de categorías
+ * @returns categoríaId - ID de la categoría que más se repite en la búsqueda
+ */
+
 export const findMostFrequent = (arr: string[]): string => {
     const category = arr.sort((a, b) => arr.filter((v) => v === a).length - arr?.filter((v) => v === b).length).pop();
     return category?.toString() || arr[0];
